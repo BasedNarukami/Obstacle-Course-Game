50baf64ae10a26d58660e4e291c7994080e615d5
BasedNarukami <100251840+BasedNarukami@users.noreply.github.com>
2026-01-25 12:42:07 -0700

    Add moveSpeed and make movement framerate independent

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
+    [SerializeField] float moveSpeed = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,12 +13,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-       float xValue = Input.GetAxis("Horizontal");
+       float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; // left and right movement, as well as make it frame rate independent
        float yValue = 0f;
-       float zValue = Input.GetAxis("Vertical");
+       float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed; // forward and backward movement, as well as make it frame rate independent
 
-
-        transform.Translate(xValue, yValue, zValue);
+       transform.Translate(xValue, yValue, zValue);
     }
 }
