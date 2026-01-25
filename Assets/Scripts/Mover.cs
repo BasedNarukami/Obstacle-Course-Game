b1ef87b7f89a5ba5141e120271e7432c1b8dadc4
BasedNarukami <100251840+BasedNarukami@users.noreply.github.com>
2026-01-25 12:28:16 -0700

    Add basic movement functionality

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,11 +3,6 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0f;
-
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +13,12 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+       float xValue = Input.GetAxis("Horizontal");
+       float yValue = 0f;
+       float zValue = Input.GetAxis("Vertical");
+
+
         transform.Translate(xValue, yValue, zValue);
     }
 }
