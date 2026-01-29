290ed3e5345aa471aee5f9ed11f5b7927f3d27d4
BasedNarukami <100251840+BasedNarukami@users.noreply.github.com>
2026-01-28 20:36:34 -0700

    Add debug function and refactor code

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,16 +7,30 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
-       float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; // left and right movement, as well as make it frame rate independent
-       float yValue = 0f;
-       float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed; // forward and backward movement, as well as make it frame rate independent
-
-       transform.Translate(xValue, yValue, zValue);
+        MovePlayer();
     }
+
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Use WASD or Arrow keys to move the player object.");
+        Debug.Log("Have fun!");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; // left and right movement, as well as make it frame rate independent
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed; // forward and backward movement, as well as make it frame rate independent
+
+        transform.Translate(xValue, yValue, zValue);
+    }
+
 }
