1bbee93b3225b3451bf628395c28d6c213796ee2
BasedNarukami <100251840+BasedNarukami@users.noreply.github.com>
2026-02-01 12:37:01 -0700

    Add script to destroy projectiles once target is reached as well as refactor code

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -9,12 +9,26 @@ public class FlyAtPlayer : MonoBehaviour
     void Start()
     {
         playerPosition = player.transform.position; // store player position at start
+        DestroyWhenReached();
     }
 
     
     void Update()
     {
-       
+        MoveTowardsPlayerPosition();
+        DestroyWhenReached();
+    }
+
+    void DestroyWhenReached()
+    {
+        if (transform.position == playerPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void MoveTowardsPlayerPosition()
+    {
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, projectileSpeed * Time.deltaTime); // move towards stored player position
     }
 }
