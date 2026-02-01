991483482af200a50fec69dcc830d32fc9940cea
BasedNarukami <100251840+BasedNarukami@users.noreply.github.com>
2026-02-01 11:54:38 -0700

    Add script to prevent scoring on same object multiple times and script for spinning obstacles

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,6 +4,10 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.black;
+        if (other.gameObject.tag == "Player") // Check if the object collided with has the tag "Player and change color to black"
+        {
+            GetComponent<MeshRenderer>().material.color = Color.black;
+            gameObject.tag = "Hit"; // Change the tag of the object to "Hit" to avoid multiple color changes.
+        }
     }
 }
